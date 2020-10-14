54f93833a35ac552640373b1212edcc845f9a7ed
Wooyang Son <sonwy102@gmail.com>
2020-10-14 15:59:28 -0400

    Added /, /hello

--- server.py
+++ server.py
@@ -17,9 +17,16 @@ AWESOMENESS = [
 @app.route('/')
 def start_here():
     """Home page."""
-
-    return "<!doctype html><html>Hi! This is the home page.</html>"
-
+    
+    return f"""
+    <!doctype html>
+    <html>
+    <body>
+      Hi! This is the home page.
+      <a href='/hello'>Go to Hello Page.</a>
+    </body>
+    </html>
+    """
 
 @app.route('/hello')
 def say_hello():
@@ -34,9 +41,29 @@ def say_hello():
       <body>
         <h1>Hi There!</h1>
         <form action="/greet">
-          What's your name? <input type="text" name="person">
-          <input type="submit" value="Submit">
-        </form>
+          <p>What's your name? <input type="text" name="person"></p>
+          
+          <div>
+            <label for="compliment">How are you feeling today?</label>
+            <select name="compliment">
+              <option value="awesome">Awesome</option>
+              <option value="terrific">Terrific</option>
+              <option value="fantastic">Fantastic</option>
+              <option value="neato">Neato</option>
+              <option value="fantabulous">Fantabulous</option>
+              <option value="wowza">Wowza</option>
+              <option value="oh-so-not-meh">Oh-so-not-meh</option>
+              <option value="brilliant">Brilliant</option>
+              <option value="ducky">Ducky</option>
+              <option value="coolio">Coolio</option>
+              <option value="incredible">Incredible</option>
+              <option value="wonderful">Wonderful</option>
+              <option value="smashing">Smashing</option>
+              <option value="lovely">Lovely</option>
+            </div><br>
+
+            <br><input type="submit" value="Submit">
+          </form>       
       </body>
     </html>
     """
@@ -48,19 +75,19 @@ def greet_person():
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
+    compliment = request.args.get("compliment")
 
-    return """
+    return f"""
     <!doctype html>
     <html>
       <head>
         <title>A Compliment</title>
       </head>
       <body>
-        Hi, {}! I think you're {}!
+        Hi, {player}! I think you're {compliment}!
       </body>
     </html>
-    """.format(player, compliment)
+    """
 
 
 if __name__ == '__main__':
